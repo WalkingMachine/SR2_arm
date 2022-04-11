fedff3ac8fb769d66ce1faf87dbe3f80be627109
S1lver <nimai.jariwala.1@ens.etsmtl.ca>
2022-04-11 15:40:20 -0400

    fixed setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 package_name = 'sr2_arm'
+robotiq_85 = 'sr2_arm/robotiq_85'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[package_name, robotiq_85],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
